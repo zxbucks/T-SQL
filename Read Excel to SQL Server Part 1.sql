ef7147fbc7d55475e60a6838fd929bf8ec8b4f43
Xu ZHANG <zxbucks@gmail.com>
2025-12-01 14:13:02 -0500

    Update and rename Load Excel from File System to SQL Server P1.sql to Read Excel to SQL Server Part 1.sql

--- Read Excel to SQL Server Part 1.sql
+++ Read Excel to SQL Server Part 1.sql
@@ -1,3 +1,4 @@
+--Read Excel from file system (network shared too) to SQL Server
 --Part 1:
 --Run below script so you have right permission
 --You only need to run below script once 
@@ -22,4 +23,5 @@ EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'DynamicParam
 EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.16.0' , N'AllowInProcess' , 1
 EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.16.0' , N'DynamicParameters' , 1
 GO
+
 
